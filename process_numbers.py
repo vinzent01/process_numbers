bf7248ec99687c78df6733adac9b7ecd141e79ed
izkeas <izkeasomito@gmail.com>
2024-06-05 14:29:45 -0300

    Updated readme.md

--- process_numbers.py
+++ process_numbers.py
@@ -1,4 +1,5 @@
 import statistics
+import os
 
 def process_numbers(numbers):
     """
@@ -10,11 +11,25 @@ def process_numbers(numbers):
     
     return total_sum, average, median
 
+def load_input():
+    f = open("input.txt", "r")
+    lines = ", ".join(f.readlines())
+    numbers_str =  lines.split(",")
+    numbers_int = [int(x.strip()) for x in numbers_str]
+    return numbers_int
+
 if __name__ == "__main__":
-    # Example usage
-    numbers = [10, 20, 30, 40, 50]
-    total_sum, average, median = process_numbers(numbers)
+    if (not os.path.exists("./input.txt")):
+        print("Error Create a input text file in : ./input.txt")
+        exit()
+
+    numbers = load_input()
+
+    if (numbers):
+        total_sum, average, median = process_numbers(numbers)
     
-    print(f"Sum: {total_sum}")
-    print(f"Average: {average}")
-    print(f"Median: {median}")
+        print(f"Sum: {total_sum}")
+        print(f"Average: {average}")
+        print(f"Median: {median}")
+    else:
+        print("Insert input separated by comma in file: input.txt")
